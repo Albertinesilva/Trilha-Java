c0ea2d8e2108a10195a7204c1fdfe268da675cd0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-01-25 17:02:44 -0300

    ConfiguracaoInterface

--- src/main/java/residenciatic18/mastermind/jogointerface/ConfiguracaoInterface.java
+++ src/main/java/residenciatic18/mastermind/jogointerface/ConfiguracaoInterface.java
@@ -1,16 +1,40 @@
 package residenciatic18.mastermind.jogointerface;
 
 public class ConfiguracaoInterface {
-    
-    public static void SelecionarConfiguracao(){
+
+    public static void menuPrincipal() {
+
+        System.out.println("\n\t[1] - CONFIGURAÇÕES");
+        System.out.println("\t[2] - JOGAR");
+        System.out.println("\t[3] - SAIR");
 
     }
 
-    public static void CriarConfiguracao(){
+    public static void menuConfiguracoes() {
+
+        System.out.println("\t[1] - CRIAR CONFIGURAÇÃO");
+        System.out.println("\t[2] - EXCLUIR CONFIGURAÇÃO");
+        System.out.println("\t[3] - SELECIONAR CONFIGURAÇÃO");
+        System.out.println("\t[0] - SAIR");
+    }
+
+    public static void menuJogar() {
+
+        System.out.println("\t[1] - INICIAR JOGO");
+        System.out.println("\t[2] - CADASTRAR SENHA");
+        System.out.println("\t[3] - INICIAR PARTIDA");
+        System.out.println("\t[0] - VOLTAR");
+    }
+
+    public static void selecionarConfiguracao() {
 
     }
 
-    public static void excluirConfiguracao(){
+    public static void criarConfiguracao() {
+
+    }
+
+    public static void excluirConfiguracao() {
 
     }
 }
